84c1f5d279651f6f3401ff0eb174f1e15f70be1e
TianKai Ma <tiankaima@163.com>
2024-12-06 10:09:06 +0800

    fix: change username to user

--- cuda/build.sh
+++ cuda/build.sh
@@ -7,13 +7,13 @@ docker pull ghcr.io/${OWNER}/cuda-11.6/latest || true
 docker build --cache-from ghcr.io/${OWNER}/cuda:latest \
   -t ghcr.io/${OWNER}/cuda:${SHA} \
   -t ghcr.io/${OWNER}/cuda:latest \
-  --build-arg USERNAME=${OWNER} \
+  --build-arg USERNAME=user \
   ./cuda
 
 docker build --cache-from ghcr.io/${OWNER}/cuda-11.6:latest \
   -t ghcr.io/${OWNER}/cuda-11.6:${SHA} \
   -t ghcr.io/${OWNER}/cuda-11.6:latest \
-  --build-arg USERNAME=${OWNER} \
+  --build-arg USERNAME=user \
   --build-arg CUDA_BASE=11.6.1-devel-ubuntu20.04 \
   ./cuda
 
