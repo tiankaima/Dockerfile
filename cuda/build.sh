e2f676acbcd29bb46043f039e37b94a26b74fbf2
TianKai Ma <tiankaima@163.com>
2025-03-21 11:30:39 +0800

    feat: use zsh, change tag push settings

--- cuda/build.sh
+++ cuda/build.sh
@@ -3,23 +3,17 @@ set -e
 
 TAGS=(
   12.4.1-cudnn-runtime-ubuntu22.04
-  12.4.1-cudnn-runtime-ubuntu20.04
-  12.1.1-cudnn8-runtime-ubuntu22.04
-  12.1.1-cudnn8-runtime-ubuntu20.04
-  11.8.0-cudnn8-runtime-ubuntu22.04
-  11.8.0-cudnn8-runtime-ubuntu20.04
 )
-``
-for tag in "${TAGS[@]}"; do
-  docker pull ghcr.io/${OWNER}/cuda-${tag} || true
 
-  docker build --cache-from ghcr.io/${OWNER}/cuda-${tag} \
-    -t ghcr.io/${OWNER}/cuda-${tag}:latest \
+for tag in "${TAGS[@]}"; do
+  docker pull ghcr.io/${OWNER}/cuda:${tag} || true
+
+  docker build -t ghcr.io/${OWNER}/cuda:${tag} \
     --build-arg CUDA_BASE=${tag} \
     ./cuda
 
-  docker push ghcr.io/${OWNER}/cuda-${tag}:latest
+  docker push ghcr.io/${OWNER}/cuda:${tag}
 
-  docker image rm ghcr.io/${OWNER}/cuda-${tag}:latest
+  docker image rm ghcr.io/${OWNER}/cuda:${tag}
   docker system prune -f
 done
