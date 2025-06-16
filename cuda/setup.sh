0965848eb9205548bf94d6719565aafc364cb06e
TianKai Ma <tiankaima@163.com>
2025-06-16 15:20:03 +0800

    fix(cuda): zshrc settings

--- cuda/setup.sh
+++ cuda/setup.sh
@@ -9,9 +9,9 @@ export USER_UID=1000
 sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
-apt update
-apt upgrade -y
-apt install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh zsh-syntax-highlighting zsh-autosuggestions
+apt-get update
+apt-get upgrade -y
+apt-get install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh zsh-syntax-highlighting zsh-autosuggestions
 rm -rf /var/lib/apt/lists/*
 
 curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh
@@ -26,3 +26,10 @@ echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/${USERNAME}
 chown -R ${USERNAME}:${USERNAME} ${MINICONDA_PATH}
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
 chsh -s /bin/zsh ${USERNAME}
+
+sudo -u user sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
+    git clone --branch 24.09.04 https://github.com/marlonrichert/zsh-autocomplete.git /home/user/.oh-my-zsh/custom/plugins/zsh-autocomplete &&
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/user/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &&
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/user/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+mv /home/user/.zshrc.pre-oh-my-zsh /home/user/.zshrc
