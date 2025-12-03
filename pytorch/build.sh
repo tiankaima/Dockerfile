56a68b5df4fbe54b7d5f4ec3e2b06093ff821a44
TianKai Ma <tiankaima@163.com>
2025-12-03 22:40:43 +0800

    pytorch: add

---
+++ pytorch/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -xeuo pipefail
+
+TAGS=(
+  2.9.1-cuda12.6-cudnn9-runtime
+  2.9.1-cuda12.8-cudnn9-runtime
+  2.9.1-cuda13.0-cudnn9-runtime
+  2.6.0-cuda12.4-cudnn9-runtime
+)
+
+for tag in "${TAGS[@]}"; do
+  docker pull ghcr.io/${OWNER}/pytorch:${tag} || true
+
+  docker build \
+    -t ghcr.io/${OWNER}/pytorch:${tag} \
+    --build-arg PYTORCH_BASE=${tag} \
+    ./pytorch
+
+  docker push ghcr.io/${OWNER}/pytorch:${tag}
+  docker image rm ghcr.io/${OWNER}/pytorch:${tag}
+  docker system prune -f
+done
