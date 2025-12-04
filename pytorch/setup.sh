514d42a30ff62ecf2891390329bdd26c8a40ad46
TianKai Ma <tiankaima@163.com>
2025-12-04 17:20:09 +0800

    pytorch: use bash

--- pytorch/setup.sh
+++ pytorch/setup.sh
@@ -3,8 +3,6 @@
 set -xeuo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
-export MINICONDA_VERSION=latest
-export MINICONDA_PATH=/opt/miniconda
 export USERNAME=user
 export USER_UID=1000
 
@@ -13,17 +11,13 @@ sed -i 's/security.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y --no-install-recommends curl wget git sudo neovim tmux less ssh zsh ncdu tree
+apt-get install -y --no-install-recommends curl wget git sudo neovim tmux less ssh ncdu tree
 rm -rf /var/lib/apt/lists/*
-
-curl -s -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh
-bash miniconda.sh -b -p ${MINICONDA_PATH}
-rm miniconda.sh
 
 userdel -rf ubuntu || true
 useradd -m ${USERNAME} --uid=${USER_UID}
 usermod -aG sudo ${USERNAME}
 echo 'user:password' | chpasswd
 echo "${USERNAME} ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/${USERNAME}
-chown -R ${USERNAME}:${USERNAME} ${MINICONDA_PATH}
+chsh -s /bin/bash ${USERNAME}
 chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}
