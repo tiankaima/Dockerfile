905412ea81d3a24ff669574587c6a717ae2b2656
TianKai Ma <tiankaima@163.com>
2025-12-09 04:04:57 +0800

    pytorch: add devel

--- pytorch/build.sh
+++ pytorch/build.sh
@@ -3,9 +3,13 @@ set -xeuo pipefail
 
 TAGS=(
   2.9.1-cuda12.6-cudnn9-runtime
+  2.9.1-cuda12.6-cudnn9-devel
   2.9.1-cuda12.8-cudnn9-runtime
+  2.9.1-cuda12.8-cudnn9-devel
   2.9.1-cuda13.0-cudnn9-runtime
+  2.9.1-cuda13.0-cudnn9-devel
   2.6.0-cuda12.4-cudnn9-runtime
+  2.6.0-cuda12.4-cudnn9-devel
 )
 
 for tag in "${TAGS[@]}"; do
